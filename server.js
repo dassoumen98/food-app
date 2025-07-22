7c370e65d7222ddd0805c6b74654325420781c0f
Soumen Das <dassoumen9832@gmail.com>
2025-07-22 13:31:33 +0530

    install pacage

--- server.js
+++ server.js
@@ -1,14 +1,20 @@
-import express from 'express'
+import express from 'express';
+import colors from 'colors';
+import cors from 'cors'
+import morgan from 'morgan';
+import  dotenv from 'dotenv'
+dotenv.config()
 
-let app = express()
+const app = express();
+app.use(cors())
+app.use(express.json())
+app.use(morgan('dev'))
 
+app.get("/", (req, res) => {
+    res.status(200).send("<h1>Welcome to Food Server App</h1>");
+});
 
-app.get("/" , (req,res)=>{
-    res.status(200).send("<h1>Welcome to Food Server App</h1>")
-})
-
-
-app.listen(8080 , ()=>{
-    console.log("Server is Running");
-    
-})
+const PORT = process.env.PORT||8080
+app.listen( PORT, () => {
+    console.log(`Server is Running ${PORT}`.bgWhite.blue);  // Will log in green
+});
